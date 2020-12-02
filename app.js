e1fdf98fa689efeda713dc78c7fc271d29288e04
reyadussalahin <reyadussalahin@gmail.com>
2020-12-02 21:24:04 +0600

    added views, static files and database support

--- app.js
+++ app.js
@@ -2,16 +2,53 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (request, response) => {
-    response.send(
-        'Hello World!'
-        + "<br>"
-        + "I am Legend!"
-        + "<br>"
-        + "Hoo, yeaaahhhh!"
-    );
+// mysql
+const mysql = require('mysql');
+
+
+// handlebars
+const exphbs  = require('express-handlebars');
+// template engine
+app.engine('.hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+// static files
+app.use(express.static('public'));
+
+
+// routes
+app.get('/', (req, res) => {
+    // create database connection
+    const conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'reyad',
+        password: '12345678',
+        database: 'express_test'
+    })
+    conn.connect();
+
+    let name = 'tumpa';
+    conn.query('SELECT name FROM person WHERE age=23', function(err, rows, fields) {
+        if(err) {
+            conn.end();
+            throw err;
+        }
+        name = rows[0]['name'];
+
+        res.render('home', {
+            layout: 'base',
+            adj: 'awesome',
+            creator: name
+        });
+    });
+    conn.end();
 });
 
+
+// start listening
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
